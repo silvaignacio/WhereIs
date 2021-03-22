2361654ecc5f8d335b41f24cf55a4226aa78a186
Ignacio Silva Silva <isilva@seiza-ti.cl>
2021-03-22 17:35:15 -0300

    Integrar http nativo

--- app/src/main/java/com/example/whereis/Ciudades.kt
+++ app/src/main/java/com/example/whereis/Ciudades.kt
@@ -3,8 +3,15 @@ package com.example.whereis
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
 import android.widget.Button
 import android.widget.Toast
+import androidx.core.app.NavUtils
+import org.json.JSONObject
+import java.io.IOException
+import java.io.InputStream
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
 
 class Ciudades : AppCompatActivity() {
 
@@ -15,11 +22,37 @@ class Ciudades : AppCompatActivity() {
         setContentView(R.layout.activity_ciudades)
 
         val santiagoButton = findViewById<Button>(R.id.santiagoButton)
-
+        var terremotos = JSONObject(obtenerTerremotos())
+        println(terremotos.getJSONArray("features"))
         santiagoButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             intent.putExtra(TAG, "santiago")
             startActivity(intent)
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun obtenerTerremotos(): String {
+
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+
+        var inputStream: InputStream? = null
+
+        try {
+            val url = URL("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02")
+            val conn = url.openConnection() as HttpsURLConnection
+            conn.requestMethod = "GET"
+            conn.connect()
+
+            inputStream = conn.inputStream
+            return inputStream.bufferedReader().use {
+                it.readText()
+            }
+        }finally {
+            if(inputStream!= null) {
+                inputStream.close()
+            }
         }
     }
 }
